3cfee5363344c5abfe1aab2d9949c591d88cc8a5
Thitiwut Somsa <tsomsa@42bangkok.com>
2024-10-05 02:33:21 +0700

    fix ex02

--- s03/ex02/request_wikipedia.py
+++ s03/ex02/request_wikipedia.py
@@ -1,5 +1,6 @@
-import wikipedia
 import sys
+import requests
+import dewiki
 
 def create_file(input: str, content: str):
     filename = input.replace(" ", "_") + ".wiki"
@@ -7,12 +8,37 @@ def create_file(input: str, content: str):
     f.write(content)
     f.close()
 
-def wiki_suggest(input):
-    search = wikipedia.search(input)
-    if len(search) > 0:
-        return search[0]
-    else:
-        return wikipedia.suggest(input)
+def wiki_search(input: str):
+    session = requests.Session()
+    url = "https://en.wikipedia.org/w/api.php"
+    params = {
+        "action": "opensearch",
+        "namespace": "0",
+        "search": input,
+        "limit": "5",
+        "format": "json"
+    }
+
+    res = session.get(url=url, params=params)
+    data = res.json()
+    if len(data[1]) == 0:
+        return
+    return data[1][0]
+
+def wiki_page_content(title: str):
+    session = requests.Session()
+    url = "https://en.wikipedia.org/w/api.php"
+    params = {
+        "page": title,
+        "action": "parse",
+        "prop": "wikitext",
+        "formatversion": "2",
+        "format": "json"
+    }
+    res = session.get(url=url, params=params)
+    data = res.json()
+    wikitext = data["parse"]["wikitext"]
+    return dewiki.from_string(wikitext)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -20,8 +46,11 @@ if __name__ == '__main__':
         sys.exit(1)
     input = sys.argv[1]
     try:
-        suggest = wiki_suggest(input)
-        page = wikipedia.page(suggest)
-        create_file(input, page.content)
+        result = wiki_search(input)
+        if result:
+            content = wiki_page_content(result)
+            create_file(input, content)
+        else:
+            create_file(input, "")
     except Exception as e:
         print('Error:', e)
