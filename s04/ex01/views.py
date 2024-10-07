9b63583ee31b5f76d618e7235838140bccca7458
Thitiwut Somsa <kizzaraiva@gmail.com>
2024-10-07 21:24:58 +0700

    s04-ex01

--- s04/ex01/views.py
+++ s04/ex01/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 def django(request):
@@ -10,5 +11,29 @@ def display(request):
     return render(request, 'display.html', context)
 
 def templates(request):
-    context = {}
+    context = {
+        'title': 'Django Template Engine',
+        'code_var': """<img src="{% static imgUrl %}" width="720" alt="My image">""",
+        'code_for': """
+<ul>
+    {% for serie in series %}
+        <li>{{serie}}</li>
+    {% endfor %}
+</ul>""",
+"code_if": """
+{% if timer.second|divisibleby:2 %}
+    <p><b style="color: green">{{timer.isoformat}}</b></p>
+{% else %}
+    <p><b style="color: red">{{timer.isoformat}}</b></p>
+{% endif %}""",
+        'timer': datetime.now(),
+        'series': [
+            'Final Fantasy V',
+            'Final Fantasy VII',
+            'Final Fantasy IX',
+            'Final Fantasy XIV',
+            'Final Fantasy XV',
+        ],
+        'imgUrl': 'images/example.png'
+    }
     return render(request, 'templates.html', context)
