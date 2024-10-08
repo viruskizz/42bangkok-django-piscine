9982cdc72895990be4ae469716a42daeebc71a29
Thitiwut Somsa <kizzaraiva@gmail.com>
2024-10-08 22:06:31 +0700

    s05-ex03

--- s05/d42/settings.py
+++ s05/d42/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'ex00',
     'ex01',
     'ex02',
+    'ex03',
 ]
 
 MIDDLEWARE = [
--- s05/d42/urls.py
+++ s05/d42/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('ex00/', include('ex00.urls')),
     path('ex01/', include('ex01.urls')),
     path('ex02/', include('ex02.urls')),
+    path('ex03/', include('ex03.urls')),
 ]
